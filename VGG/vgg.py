183f1d0d331f5035abcc1c78135fcebfe89a3805
Hyeonseong Jeon <cutz309@gmail.com>
2019-04-02 21:53:28 +0900

    Create vgg.py

---
+++ VGG/vgg.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from keras import layers, models, optimizers, initializers
+
+origin_dir = "d:/data/dogs-vs-cats/train"
+base_dir = "d:/data/dnc"
