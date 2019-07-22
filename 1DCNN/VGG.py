28f3a6af084afc00704c32ba36a4d8842cbbd32e
Hyeonseong Jeon <cutz309@gmail.com>
2019-07-22 11:25:01 +0900

    Update

---
+++ 1DCNN/VGG.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import itertools
+from sklearn.preprocessing import RobustScaler
+from sklearn.model_selection import train_test_split
+from keras.models import Model, Sequential, load_model
+from keras.layers import Input, Embedding, LSTM, GRU, Conv1D, Conv2D, MaxPooling1D, GlobalAveragePooling1D, Dense, add
+from keras.layers import Dropout, Lambda, Bidirectional, Flatten, BatchNormalization, Activation, SeparableConv1D
+from keras.layers import ZeroPadding1D, ReLU
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, ReduceLROnPlateau
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from tqdm import tqdm, trange
+from keras import backend as K
+from keras.optimizers import Adam
+
+
+def VGG1D(input_shape, **kwargs):
+    img_input = Input(shape=input_shape)
+
+    x = Conv1D(filters=64, kernel_size=3, strides=1, padding='same', use_bias=False)(img_input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    x = Conv1D(filters=128, kernel_size=3, strides=1, padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    x = MaxPooling1D(pool_size=3, strides=2, padding='same')(x)
+    
+    x = Conv1D(filters=256, kernel_size=3, strides=1, padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    x = Conv1D(filters=256, kernel_size=3, strides=1, padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    x = MaxPooling1D(pool_size=2, strides=2, padding='valid')(x)
+    
+    x = Conv1D(filters=512, kernel_size=3, strides=1, padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    x = Conv1D(filters=512, kernel_size=3, strides=1, padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    x = Conv1D(filters=512, kernel_size=3, strides=1, padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    x = MaxPooling1D(pool_size=2, strides=2, padding='valid')(x)
+    
+    x = Flatten()(x)
+    x = Dense(4096)(x)
+    x = Activation('relu')(x)
+    x = Dropout(0.5)(x) 
+
+    out = Activation('relu')(x)
+    
+    model = Model(img_input, out)
+    return model
