d268cbcf398d052bfa07eddb188dd157bd8087dd
Hyeonseong Jeon <cutz309@gmail.com>
2019-09-14 18:28:59 +0900

    Update stochasticdepth.py

--- StochasticDepth/stochasticdepth.py
+++ StochasticDepth/stochasticdepth.py
@@ -3,9 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
-def conv3x3(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+def conv3x3(in_planes, out_planes, strides=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=strides, padding=1, bias=False)
 
-def conv1x1(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
+def conv1x1(in_planes, out_planes, strides=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=strides, bias=False)
